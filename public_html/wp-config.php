49cb28b658b8b7123fcd02a6fffe5b0dbe4cdac8
trant017 <trant017@gmail.com>
2018-08-22 22:53:12 -0400

    made path relative

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -3,11 +3,11 @@ define('WP_ENVIRONMENT', 'LIVE');
 
 switch (WP_ENVIRONMENT) {
     case 'LOCAL':
-        require_once('../wp-config-local.php');
+        require_once('/home2/tonystr4/wp-config-local.php');
         return;
     case 'LIVE':
-        require_once('../wp-config-live.php');
+        require_once('/home2/tonystr4/wp-config-live.php');
         return;
     default:
-        require_once('../wp-config-live.php');
+        require_once('/home2/tonystr4/wp-config-live.php');
 }
